5b161feea619456d49ea240c1d0986977a21aa1d
andrkrsh <akrishevskiy@gmail.com>
2024-04-16 19:46:55 +0200

    sdf

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,13 @@
-const getElementWidth = (content, padding, border) => { //mama ama gensta
-    const slimShady = parseFloat(padding) * 2 + parseFloat(border) * 2 + parseFloat(content);
-    return slimShady;
+const checkForSpam = (message) => {
+    const check = message.toLowerCase();
+    return check.includes('spam') || check.includes('sale');
 };
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
-console.log(getElementWidth("20px", "10px", "10px"));
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
